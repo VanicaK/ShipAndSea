fd1e8f1aab955bedc639b3badaf1716a7951926a
VijayWasTaken <88245937+VijayWasTaken@users.noreply.github.com>
2021-08-06 19:16:56 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var sea, seaMove;
+var ship, shipMove;
+
+function preload() {
+  shipMove = loadAnimation("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
+  seaMove = loadImage("sea.png");
+
+}
+
+function setup() {
+  createCanvas(400, 400);
+  sea = createSprite(0, 200, 400, 400);
+  sea.addImage("se", seaMove);
+  ship = createSprite(200, 250, 100, 100);
+  ship.addAnimation("sp", shipMove);
+  ship.scale = 0.3;
+  sea.velocityX = -2;
+}
+
+
+function draw() {
+  background("blue");
+  if (sea.x < 0) {
+    sea.x = sea.width / 2;
+  }
+  drawSprites();
+}
